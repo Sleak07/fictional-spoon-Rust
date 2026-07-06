f1faf1cc7170eae56548e01253b5bd7c363909d7
Bimal Mallick <ghostridergothan8460@gmail.com>
2026-07-06 19:33:26 +0530

    Half way done going to do tommorow

--- brainfuck-interpreter/src/main.rs
+++ brainfuck-interpreter/src/main.rs
@@ -1,3 +1,32 @@
+fn build_bracket_map(program: &str) -> Vec<usize> {
+    let bytes = program.as_bytes();
+    let len = bytes.len();
+    let mut map = vec![0usize; len];
+    let mut stack: Vec<usize> = Vec::new();
+
+    for i in 0..len {
+        match bytes[i] {
+            b'[' => {
+                stack.push(i);
+            }
+            b']' => {
+                let open = stack.pop().expect("Unmatched ]");
+                map[open] = i;
+                map[i] = open;
+            }
+            _ => {}
+        }
+    }
+    if !stack.is_empty() {
+        panic!("Unmatched [");
+    }
+    map
+}
+
 fn main() {
-    println!("Hello, world!");
+    let program = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+
+    let mut tape = [0u8; 30000];
+    let mut dp: usize = 0;
+    let mut pc: usize = 0;
 }
